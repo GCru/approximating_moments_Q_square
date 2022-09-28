311fd722e648aa9c0959968fd0dec1c7328e3d5d
fundskill <gerhard@cruywagen.com>
2022-09-28 15:22:47 +0200

    First commit

--- active_risk_distribution.py
+++ active_risk_distribution.py
@@ -1,13 +1,14 @@
+from fundskill_utilities.setup_python_file_with_django import *  # this sets up the django environment
 
 
-
-
-
+from active_risk_utilities import set_up_in_sample_month_end_list
+from calculate_active_risk_for_random_funds_procedure import calculate_small_random_fund_results_and_write_to_file
 
 if __name__ == '__main__':
 	
 	date_range = set_up_in_sample_month_end_list()
-	
+	#only take last date
+	date_range =date_range[-1:]
 	# date_range =[start_date]
 	
 	# Set up benchmarks of the funds to consider
@@ -20,7 +21,7 @@ if __name__ == '__main__':
 	benchmark_list = ['S&P 500 TR USD']
 	
 	for benchmark_name in benchmark_list:
-		results_file_name = 'random_10_10000_funds_risk_forecast_' + benchmark_name + '.csv'
+		results_file_name = 'random_50_10000_funds_risk_distribution_' + benchmark_name + '.csv'
 		
 		calculate_small_random_fund_results_and_write_to_file(
-			benchmark_name, date_range, results_file_name, number_of_months=60, number_of_stocks=10)
+			benchmark_name, date_range, results_file_name, number_of_months=60, number_of_stocks=50,calc_method=['clipped'])
