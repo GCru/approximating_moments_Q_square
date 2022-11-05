505fc0c75284b1b09c402ab36fcc29bd697230d6
fundskill <gerhard@cruywagen.com>
2022-11-05 23:23:20 +0200

    First commit

--- test_marchenko_pastur.py
+++ test_marchenko_pastur.py
@@ -16,7 +16,9 @@ def create_random_n_by_T_matrix(n,T):
 
 def create_cov_matrix(X,n ):
 
-	return numpy.cov(X)
+	#return numpy.cov(X)
+	X[0,:]*=0.1
+	X[1,:]=0.1
 
 	return numpy.matmul(X,numpy.transpose(X) ) / T
 
@@ -29,18 +31,19 @@ if __name__ == '__main__':
 	#print(v_square, w_times_v)
 	#exit()
 	
-	n=200
-	T=25
+	n=20
+	T=2
 	
 	X = create_random_n_by_T_matrix(n,T)
+	
+
 	
 	Y= create_cov_matrix(X,n)
 	print(Y.shape)
 	
 	
-	
 	l , _ = numpy.linalg.eig(Y)
-	print('Maximum eigenvalue',max(l), 'vs Marchenko-Pastur', 4* (1+(n/T)**0.5)**2)
+	print('Maximum eigenvalue',max(l), 'vs Marchenko-Pastur', 4* (1+(n/T)**0.5)**2, 'vs minimum', n*4/T)
 	
 	D = numpy.zeros((n, n))
 	numpy.fill_diagonal(D, 1)
@@ -55,3 +58,18 @@ if __name__ == '__main__':
 	
 	
 	print('Average of all entries sqaured', numpy.average(Y)-1/n )
+	
+	list_1=[[3],[2],[2]]
+	
+	vector_1 =numpy.array(list_1)
+	
+	print(vector_1)
+	
+	A= numpy.matmul(vector_1,numpy.transpose(vector_1))
+	
+	print(A)
+	
+	l, _ = numpy.linalg.eig(A)
+	
+	print(max(l))
+	
