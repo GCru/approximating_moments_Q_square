160803139f248c327de774d35e6f4b8fc16b1558
fundskill <gerhard@cruywagen.com>
2022-12-02 19:38:26 +0200

    First commit

--- weighted_chi_square_sum.py
+++ weighted_chi_square_sum.py
@@ -2,35 +2,47 @@
 import numpy
 from momentchi2 import hbe
 
+import math
 
-coeff_list=[0.4,0.3,0.3]
-#coeff_list=[0.4,0.3,0.3]
-t=2
-coef_list=numpy.array(coeff_list)
-x=hbe(coeff=coeff_list, x=t)
-print(x)
+from scipy import stats, special
+
+def cumulant_infinite_gamma(k,eigenvalues_array):
+	
+	n= len(eigenvalues_array)
+	kappa = 2**(k-1)*(math.factorial(k-1))*numpy.dot(eigenvalues_array, eigenvalues_array)/n**k
+	
+	return kappa
 
 
-coeff_list=[0.4,0.0001,0.0001]
-t=2
-coef_list=numpy.array(coeff_list)
-x=hbe(coeff=coeff_list, x=t)
-print(x)
 
-exit()
-# should give value close to 0.95, actually 0.94908
-coeff_list=[0.8,0.1,0.1]
-#coeff_list=[0.4,0.3,0.3]
-t=4
-coef_list=numpy.array(coeff_list)
-x=hbe(coeff=coeff_list, x=t)
 
-approx = t**(1/3) -(1-(2/9)*numpy.dot(coef_list, coef_list))
-approx=approx/( ((2/9)*numpy.dot(coef_list, coef_list))**0.5)
-print('*****', approx)
-print(numpy.dot(coef_list, coef_list),numpy.dot(coeff_list,coeff_list)**0.5)
 
-print(x)
+if __name__ == '__main__':
+	# should give value close to 0.95, actually 0.94908
+	coeff_list=[0.8,0.1,0.1]
+	
+	
+	#coeff_list=[0.4,0.3,0.3]
+	
+	coef_list=numpy.array(coeff_list)
+
+	
+	t=2
+
+
+	# exact cdf
+	x=hbe(coeff=coeff_list, x=t)
+	print('Exact cdf', x)
+
+	# approx cdf
+	approx = t**(1/3) -(1-(2/9)*numpy.dot(coef_list, coef_list))
+	approx=approx/( ((2/9)*numpy.dot(coef_list, coef_list))**0.5)
+	print('Approx cdf ', stats.norm.cdf(approx))
+
+	# Dot product of coefficients or eigenvalues
+	print("Dot product of eigenvalues", numpy.dot(coef_list, coef_list),numpy.dot(coeff_list,coeff_list)**0.5)
+
+
 exit()
 coeff_list=[1/10]*10
 x=hbe(coeff=coeff_list, x=2)
@@ -48,3 +60,22 @@ coeff_list=[0.5,0.11,0.09,0.1,0.1,0.1]
 print(coeff_list, sum(coeff_list))
 x=hbe(coeff=coeff_list, x=2)
 print(x)
+
+
+
+
+coeff_list=[0.4,0.3,0.3]
+#coeff_list=[0.4,0.3,0.3]
+t=2
+coef_list=numpy.array(coeff_list)
+x=hbe(coeff=coeff_list, x=t)
+print(x)
+
+
+coeff_list=[0.4,0.0001,0.0001]
+t=2
+coef_list=numpy.array(coeff_list)
+x=hbe(coeff=coeff_list, x=t)
+print(x)
+
+exit()
