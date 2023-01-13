5f048f2f03d6e690447519f66a7c2efb61f9ea3a
fundskill <gerhard@cruywagen.com>
2023-01-13 18:34:15 +0200

    First commit

--- check_taylor_approximations.py
+++ check_taylor_approximations.py
@@ -1,5 +1,5 @@
 
-import numpy
+import numpy, math
 from datetime import date
 from statistics import stdev, mean
 from math import pi, log, sqrt
@@ -36,13 +36,84 @@ def create_covariance_matrix(sigma, rho, n):
 	for idx in range(n):
 		V[idx, idx] = sigma * sigma
 	
-	w, v = numpy.linalg.eig(V)
-	print('Max eigenvalue:', max(w), ' Min eigenvalue', min(w))
-	print(w)
-	# print(w)
-	# print(w)
-	
 	return V
+
+def monte_carlo_moments_wVw_and_sqrt_wVw(V, iterations=1000):
+	
+	n=numpy.shape(V)[0]
+	
+	I = numpy.zeros((n, n))
+	numpy.fill_diagonal(I, 1)
+	wVw = []
+	wVw_sqrt = []
+
+	for count in range(iterations):
+		
+		w = numpy.random.default_rng().multivariate_normal(numpy.zeros(n), (1 / n) * I)
+		
+		wVw.append(numpy.dot(w, numpy.dot(V, w)))
+		wVw_sqrt.append(wVw[-1] ** 0.5)
+		
+	mean_wVw_monte_carlo = numpy.mean(wVw)
+	mean_sqrt_wVw_monte_carlo = numpy.mean(wVw_sqrt)
+	
+	var_wVw_monte_carlo = numpy.var(wVw)
+	var_sqrt_wVw_monte_carlo =numpy.var(wVw_sqrt)
+	
+	taylor_support_min_monte_carlo = min(wVw)
+	taylor_support_max_monte_carlo = max(wVw)
+	return mean_wVw_monte_carlo, var_wVw_monte_carlo, mean_sqrt_wVw_monte_carlo, var_sqrt_wVw_monte_carlo, \
+		taylor_support_min_monte_carlo, taylor_support_max_monte_carlo
+
+def calculate_algebraic_moments_wVw(V):
+	
+	n = numpy.shape(V)[0]
+	
+	mean_wVw_algebraic = numpy.trace(V) / n
+	
+	var_wVw_algebraic = 2 * numpy.average(numpy.square(V))
+
+	# or in term of rho and sigma
+	# 2 * ((rho ** 2) * (n - 1) * n + (sigma ** 2) * n) / (n * n)
+	
+	return mean_wVw_algebraic, var_wVw_algebraic
+
+
+
+
+def calculate_cumulant(k, eigenvalue_list ):
+	
+	n=len(eigenvalue_list)
+	kappa = 2**(k-1)*math.factorial(k-1) / n**k
+
+	sum_product = 0
+	for eigenvalue in eigenvalue_list:
+		sum_product = sum_product + eigenvalue**k
+		
+	return kappa*sum_product
+
+
+def calculate_taylor_mean_sqrt_wVw(V):
+	n = numpy.shape(V)[0]
+	
+	mean_sqrt_wVw_taylor = (numpy.trace(V) / n) ** 0.5 - (1.0 / 8.0) * (numpy.trace(V) / n) ** (
+		-1.5) * 2 * numpy.average(
+		numpy.square(V))
+	
+	return mean_sqrt_wVw_taylor
+
+
+def calculate_upper_bound_mean_sqrt_wVw(V):
+	
+	n = numpy.shape(V)[0]
+	
+	return   (numpy.trace(V) / n) ** 0.5
+
+
+def calculate_taylor_suport_algebraic(V):
+	
+	n = numpy.shape(V)[0]
+	return 0, 2 * numpy.trace(V)
 
 
 if __name__ == '__main__':
@@ -63,12 +134,39 @@ if __name__ == '__main__':
 	# exit()
 	
 	# Set up covariance matrix of size n
-	n = 10
-	rho = 0 # set to zero for all eigenvalues the same and set to one for only one positve eigenvalue
+	n = 100
+	rho = 1 # set to zero for all eigenvalues the same and set to one for only one positve eigenvalue
 	sigma = 0.1
 	V = create_covariance_matrix(sigma, rho, n)
 	print(V)
-	# V[1,1] =0.4
+	#V[1,1] =0.4
+	
+	# Calculate eigenvalues
+	eigenvalue_list, _ = numpy.linalg.eig(V)
+	print('Max eigenvalue:', max(eigenvalue_list), ' Min eigenvalue', min(eigenvalue_list))
+	print(eigenvalue_list)
+	
+	# Calculate cumulant
+	k=3
+	print('Cumulant ',k, ': ', calculate_cumulant(k, eigenvalue_list))
+	
+	# Calculate mean and variance using monte carlo
+	mean_wVw_monte_carlo, var_wVw_monte_carlo, mean_sqrt_wVw_monte_carlo, var_sqrt_wVw_monte_carlo,\
+		taylor_support_min_monte_carlo, taylor_support_max_monte_carlo = monte_carlo_moments_wVw_and_sqrt_wVw(V)
+	
+	
+	print(mean_wVw_monte_carlo, var_wVw_monte_carlo, mean_sqrt_wVw_monte_carlo, var_sqrt_wVw_monte_carlo)
+	# Calculate algebraic mean and varince of wVw
+	mean_wVw_algebraic, var_wVw_elgebraic = calculate_algebraic_moments_wVw(V)
+	
+	print(mean_wVw_algebraic, var_wVw_monte_carlo)
+	
+	mean_sqrt_wVw_taylor = calculate_taylor_mean_sqrt_wVw(V)
+	print(mean_sqrt_wVw_taylor)
+	
+	print('Percentage error taylor vs monte carlo for expected value')
+	print(100* (mean_sqrt_wVw_taylor-mean_sqrt_wVw_monte_carlo)/(mean_sqrt_wVw_monte_carlo),'%')
+	exit()
 	
 	I = numpy.zeros((n, n))
 	numpy.fill_diagonal(I, 1)
