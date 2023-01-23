d4a43ba65d3fe85b439db9dba3049cb3114d1d29
fundskill <gerhard@cruywagen.com>
2023-01-23 18:53:48 +0200

    First commit

--- radius_of_convergence_sqrt.py
+++ radius_of_convergence_sqrt.py
@@ -1,6 +1,7 @@
 import numpy
 
 from bokeh.plotting import figure, show
+from bokeh.layouts import row
 from bokeh.models import Range1d
 
 x = numpy.linspace(0, 5, 500)
@@ -14,24 +15,31 @@ taylor_4 = taylor_3+(3/48)*x0**(-5/2)*(x-x0)**3
 taylor_5 = taylor_4-(15/(16*24))*x0**(-7/2)*(x-x0)**4
 taylor_6 = taylor_5+ (105/(32*120))*x0**(-9/2)*(x-x0)**5
 
-p = figure(width=800, height=350, title="", tools="",
+p_left = figure(width=400, height=350, title="", tools="",
+           toolbar_location=None)
+p_left.x_range=Range1d(0,4)
+p_left.y_range=Range1d(0,3.2)
+
+p_left.line(x, y, color="navy", alpha=0.4, line_width=5)
+#p.line(x, taylor_1, color="navy", alpha=0.4, line_width=2)
+p_left.line(x, taylor_2, color="navy", alpha=0.4, line_width=2, line_dash="dashed")
+p_left.line(x, taylor_3, color="navy", alpha=0.4, line_width=2, line_dash="solid")
+p_left.line(x, taylor_4, color="navy", alpha=0.4, line_width=2, line_dash='dotted')
+
+
+
+p_right = figure(width=400, height=350, title="", tools="",
            toolbar_location=None)
 
-p.x_range=Range1d(0,4)
-p.y_range=Range1d(0,3.2)
-
-p.line(x, y, color="navy", alpha=0.4, line_width=4)
-#p.line(x, taylor_1, color="navy", alpha=0.4, line_width=2)
-p.line(x, taylor_2, color="navy", alpha=0.4, line_width=2)
-p.line(x, taylor_3, color="navy", alpha=0.4, line_width=2)
-p.line(x, taylor_4, color="navy", alpha=0.4, line_width=2)
-#p.line(x, taylor_5, color="navy", alpha=0.4, line_width=2)
-#p.line(x, taylor_6, color="navy", alpha=0.4, line_width=2)
+p_right.line(x, y, color="navy", alpha=0.4, line_width=5)
+p_right.line(x, taylor_4, color="navy", alpha=0.4, line_width=2, line_dash='dotted')
+p_right.line(x, taylor_5, color="navy", alpha=0.4, line_width=2, line_dash='dotdash')
+p_right.line(x, taylor_6, color="navy", alpha=0.4, line_width=2, line_dash='dashdot')
 
 
 
-p.background_fill_color = "#efefef"
+p_left.background_fill_color = "#efefef"
 #p.xaxis.fixed_location = 0
 #p.yaxis.fixed_location = 0
 
-show(p)
+show(row(p_left,p_right))
