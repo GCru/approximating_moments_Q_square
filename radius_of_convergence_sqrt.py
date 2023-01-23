b8d7c72d35ac6bb918514b9e81f410e456300156
fundskill <gerhard@cruywagen.com>
2023-01-23 18:38:03 +0200

    First commit

--- radius_of_convergence_sqrt.py
+++ radius_of_convergence_sqrt.py
@@ -1,6 +1,7 @@
 import numpy
 
 from bokeh.plotting import figure, show
+from bokeh.models import Range1d
 
 x = numpy.linspace(0, 5, 500)
 y = x**0.5
@@ -14,20 +15,23 @@ taylor_5 = taylor_4-(15/(16*24))*x0**(-7/2)*(x-x0)**4
 taylor_6 = taylor_5+ (105/(32*120))*x0**(-9/2)*(x-x0)**5
 
 p = figure(width=800, height=350, title="", tools="",
-           toolbar_location=None, match_aspect=False)
+           toolbar_location=None)
+
+p.x_range=Range1d(0,4)
+p.y_range=Range1d(0,3.2)
 
 p.line(x, y, color="navy", alpha=0.4, line_width=4)
-p.line(x, taylor_1, color="navy", alpha=0.4, line_width=2)
+#p.line(x, taylor_1, color="navy", alpha=0.4, line_width=2)
 p.line(x, taylor_2, color="navy", alpha=0.4, line_width=2)
 p.line(x, taylor_3, color="navy", alpha=0.4, line_width=2)
 p.line(x, taylor_4, color="navy", alpha=0.4, line_width=2)
-p.line(x, taylor_5, color="navy", alpha=0.4, line_width=2)
-p.line(x, taylor_6, color="navy", alpha=0.4, line_width=2)
+#p.line(x, taylor_5, color="navy", alpha=0.4, line_width=2)
+#p.line(x, taylor_6, color="navy", alpha=0.4, line_width=2)
 
 
 
 p.background_fill_color = "#efefef"
 #p.xaxis.fixed_location = 0
-p.yaxis.fixed_location = 0
+#p.yaxis.fixed_location = 0
 
 show(p)
